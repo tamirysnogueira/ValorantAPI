d39d425474a11ecce63c79a4c4d48393e59ff2f1
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-03-22 17:16:04 -0300

    new Repository

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tl1 = new TimelineMax();
+    tl1
+        .fromTo(`#animation`, 1, {height: '100%', width: '100%'},{
+            height: '400%', width: '200%',  delay: 3, ease: Expo.easeInOut
+        })
+
+        .fromTo(`#pageInitial`, 1, {display: 'flex'},{
+            display: 'none',  delay: 0, ease: Expo.easeInOut
+        })
+
+        .fromTo('main', 1, 
+        {opacity: '0'},
+        {opacity: 1, ease: Power3.easeInOut
+        }, '-=0.7')
+
+        .fromTo('body', 1, 
+        {overflowY: 'hidden'},
+        {overflowY: 'auto', ease: Power3.easeInOut
+        })
+
+            .fromTo('#logoAndInfo', 0.7, 
+                { y: 50, opacity: 0,},
+                { y: 0, opacity: 1, ease: Back.easeOut.config(1.7)
+            }, '-=0.3')
+})
+
